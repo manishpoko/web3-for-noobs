1c40a7e7461542f08ed2e119be125940610e1909
manishpoko <manishpokhrel313@gmail.com>
2025-12-09 10:14:23 +0530

    auth

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express"
 import type { Request, Response } from "express"
+import prisma from '@prisma/client'
+
+import bcrypt from 'bcrypt'
+import type { PrismaClient } from "@prisma/client/extension";
 
 const router = Router();
 
@@ -10,5 +14,21 @@ router.get('/health', (req: Request, res: Response) => {
 router.post('/login', (req: Request, res: Response)=> {
     res.json({message: "this is the login page"})
 })
+
+
+//defining the register endpoint, wth password hashing added -
+
+router.post('/register', (req, res) => {
+const createUser = () => {
+    
+}
+
+
+})
+
+
+
+
+
 
 export default router;
