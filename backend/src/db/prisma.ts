60a08d9900ab63358461fb8a8429809372163408
manishpoko <manishpokhrel313@gmail.com>
2025-12-11 13:27:34 +0530

    added mandatory adapter for prisma 7

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,8 +1,19 @@
-import { PrismaClient } from "../generated/prisma/client.ts";
+// src/db/prisma.ts
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/index.js';
 
- const prisma = new PrismaClient();
+// 1. Create the standard Postgres connection pool 
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ 
+  connectionString 
+});
+
+// 2. Create the Prisma Adapter (prisma 7 made this mandatory)
+const adapter = new PrismaPg(pool);
+
+// 3. Pass the adapter to Prisma (This satisfies the "adapter" requirement!)
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
-
-
-
--- backend/src/db/user.ts
+++ backend/src/db/user.ts
@@ -5,7 +5,27 @@ import bcrypt from 'bcrypt'
 const prisma = new PrismaClient();
 const saltRounds = 10;
 
+interface SignupInput {
+    name: string;
+    username: string;
+    password: string
+}
 
+export async function createuser(input:SignupInput) {
+    const {username, password, name} = input;
+
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            password: hashedPassword,
+            name,
+        }
+    });
+    return newUser;
+}
 
 
 
