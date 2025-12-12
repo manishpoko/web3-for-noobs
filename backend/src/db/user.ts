f8b65cf117550dc6f56c5cdf178a192b7402e24f
manishpoko <manishpokhrel313@gmail.com>
2025-12-12 14:56:33 +0530

    checkpoint - login, jwt token issuance and error handling

--- backend/src/db/user.ts
+++ backend/src/db/user.ts
@@ -1,8 +1,16 @@
+
+
+
+//this contains the signup and login logic
+
+
+
 import { PrismaClient } from "../generated/prisma/client.js";
 
 import bcrypt from "bcrypt";
 const saltRounds = 10;
 
+import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient();
 
 interface SignupInput {
@@ -34,23 +42,31 @@ export async function loginUser(input: LoginInput) {
     where: {
       email: input.email,
     },
+    //the select logic selects only these specified fields as attributes for the main variable
     select: {
       email: true,
       password: true,
+      id: true
     },
   });
 
   if (!emailExists) {
-    return "invalid email or password"
-  }
+    throw new Error("invalid email") 
+  } 
 
   //comparing the user password with the database one
 
   const match = await  bcrypt.compare(password, emailExists.password);
     if (match) {
-      //password is correct, proceed to login
+      //password is correct, proceed to login - jwt sign here:
+      const payload = {userId: emailExists.id} //assigning the unique user id fetched during login (using select)
+
+      const secretKey = process.env.JWT_SECRET!;
+
+      const token = jwt.sign(payload, secretKey, {expiresIn: "1h"})
+      return token; //this is imp. the entire function basically is to return the jwt token here    
     } else {
-      return "invalid password";
+      throw new Error("invalid password") ;
     }
 
 
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,17 +1,39 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
 import prisma from "../db/prisma.ts";
-
 import bcrypt from "bcrypt";
-
+import { loginUser } from "../db/user.ts";
+import jwt from 'jsonwebtoken'
 const router = Router();
 
 router.get("/health", (req: Request, res: Response) => {
   res.json({ message: "authroutes check" });
 });
 
-router.post("/login", (req: Request, res: Response) => {
-  res.json({ message: "this is the login page" });
+router.post("/login", async (req: Request, res: Response) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email || !password){
+    return res.status(400).json({
+      message: "error receiving the login details"
+    })
+  }
+
+  try {
+      const result = await loginUser({email, password});
+      return res.status(200).json(result)
+    
+  } catch (error){
+    return res.status(400).json({message: (error as Error).message})
+    
+  }
+
+  
+  //jwt verification will be done using middlewares, this is just signing and ensuring correct routing
+
+
+
+
 });
 
 //defining the register endpoint, wth password hashing added -
