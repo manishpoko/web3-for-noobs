855409ce40fa1a36c68da4b3dc02eae0533f9cb6
manishpoko <manishpokhrel313@gmail.com>
2025-12-12 09:54:11 +0530

    code cleaning

--- backend/src/db/user.ts
+++ backend/src/db/user.ts
@@ -1,30 +1,29 @@
-import { PrismaClient } from '../generated/prisma/client.js'
+
+import { PrismaClient } from "../generated/prisma/client.js";
 
 import bcrypt from 'bcrypt'
-
-const prisma = new PrismaClient();
 const saltRounds = 10;
 
+const prisma = new PrismaClient();
+
 interface SignupInput {
-    name: string;
     username: string;
-    password: string
+    password: string;
+    name?: string
 }
 
-export async function createuser(input:SignupInput) {
-    const {username, password, name} = input;
-
-
+export async function createuser(input: SignupInput) {
+    const {username, password, name} = input
     const hashedPassword = await bcrypt.hash(password, saltRounds)
-
     const newUser = await prisma.user.create({
-        data: {
-            username,
-            password: hashedPassword,
-            name,
-        }
-    });
-    return newUser;
+        data: {username: username,
+        password: hashedPassword,
+        name: name}
+        });
+
+        return newUser;
+
+
 }
 
 
