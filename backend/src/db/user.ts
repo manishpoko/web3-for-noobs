c8ff3a3d0d9e0069c791a8b65180ee891d6b0721
manishpoko <manishpokhrel313@gmail.com>
2025-12-12 12:17:46 +0530

    signup and login logic using bcrypt

--- backend/src/db/user.ts
+++ backend/src/db/user.ts
@@ -1,40 +1,58 @@
-
 import { PrismaClient } from "../generated/prisma/client.js";
 
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 const saltRounds = 10;
 
 const prisma = new PrismaClient();
 
 interface SignupInput {
-    username: string;
-    password: string;
-    name?: string
+  email: string;
+  password: string;
+  username?: string;
+}
+
+interface LoginInput {
+  email: string;
+  password: string;
+  username?: string;
 }
 
 export async function createuser(input: SignupInput) {
-    const {username, password, name} = input
-    const hashedPassword = await bcrypt.hash(password, saltRounds)
-    const newUser = await prisma.user.create({
-        data: {username: username,
-        password: hashedPassword,
-        name: name}
-        });
+  const { email, password, username } = input;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const newUser = await prisma.user.create({
+    data: { email: email, password: hashedPassword, username: username },
+  });
 
-        return newUser;
+  return newUser;
+}
+
+export async function loginUser(input: LoginInput) {
+  const { email, password } = input;
+
+  const emailExists = await prisma.user.findUnique({
+    where: {
+      email: input.email,
+    },
+    select: {
+      email: true,
+      password: true,
+    },
+  });
+
+  if (!emailExists) {
+    return "invalid email or password"
+  }
+
+  //comparing the user password with the database one
+
+  const match = await  bcrypt.compare(password, emailExists.password);
+    if (match) {
+      //password is correct, proceed to login
+    } else {
+      return "invalid password";
+    }
+
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
