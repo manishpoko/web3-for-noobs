f9a5334fd9a04b45be73fc99e717a85a958115b9
manishpoko <manishpokhrel313@gmail.com>
2025-12-13 20:37:03 +0530

    completed route securing with authMiddleware

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,42 +1,45 @@
 import type { Request, Response, NextFunction } from "express";
 
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "a_fallback_secret_key"
+const JWT_SECRET = process.env.JWT_SECRET || "a_fallback_secret_key";
 
+//default Request from express doesnt have an attribute named userId, so we create our own interface extending it-
 
-export interface AuthRequest extends Request{
-    userId?: string
+export interface AuthRequest extends Request {
+  userId?: string;
 }
 
-export function authMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization;
+export function authMiddleware(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
 
-    if(!authHeader) {
-        return res.status(400).json({message: "access denied, no token provided"})
-    }
+  if (!authHeader) {
+    return res
+      .status(400)
+      .json({ message: "access denied, no token provided" });
+  }
 
+  const token = authHeader.split(" ")[1];
 
-    const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res
+      .status(400)
+      .json({ message: "error: malformed token; access denied!" });
+  }
 
-    if(!token) {
-        return res.status(400).json({message: "error: malformed token; access denied!"})
-    }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+    console.log("decoded token payload - ", decoded);
 
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET) as {id: string};
+    req.userId = decoded.userId;
+    console.log("set req.userId to - ", req.userId);
 
-        req.userId = decoded.id;
-
-        
-        next()
-
-
-    } catch (error) {
-        return res.status(403).json({ message: "Invalid or expired token." });
-
-        
-    }
-
-
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid or expired token." });
+  }
 }
--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 import type { Request, Response } from "express";
 
 import { createPost } from "../db/post.ts";
+import { authMiddleware } from "../middleware/authMiddleware.ts";
+import type { AuthRequest } from "../middleware/authMiddleware.ts";
 
 const router = Router();
 
 
 // full path for the below router  will be - /api/posts/create
-router.post('/create', async (req: Request, res: Response) => {
+router.post('/create', authMiddleware, async (req: AuthRequest, res: Response) => {
     const title = req.body.title;
     const content = req.body.content;
-    const authorId = req.body.authorId;
+    const authorId = req.userId!; //we get the userId as a token that was decoded using authMiddleware
 
     if(!title || !content || !authorId ) {
         return res.status(400).json({
@@ -26,9 +28,6 @@ router.post('/create', async (req: Request, res: Response) => {
         return res.status(400).json({message: 'error posting!'})
         
     }
-
-
-
 
 
 })
